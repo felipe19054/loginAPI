a83140ff2f49b5d56e2fe28cabe338ec79ac0b69
manuel <manuelfelipe1713@gmail.com>
2024-09-14 22:20:34 -0500

    Registro, validación, login, token, actualización

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const conectarDB =require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+// Conectar base de datos
+conectarDB();
+
+// Crear una instancia de Express
+const app = express();
+
+// Middleware para analizar el cuerpo de las solicitudes
+app.use(express.json());
+
+// Definir una ruta de prueba
+app.get('/', (req, res) => {
+    res.send('Hola mundo Adso 2024');
+});
+
+// Usar rutas de autenticación
+app.use('/api/auth', authRoutes);
+
+// Configurar el puerto en el que escuchará el servidor 
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
