e8ea6f5575418759f6d424df5bbe7eccc4ee7365
manuel <manuelfelipe1713@gmail.com>
2024-09-17 11:30:22 -0500

    Comentarios

--- config/db.js
+++ config/db.js
@@ -1,17 +1,23 @@
+// Importamos la librería mongoose para trabajar con MongoDB
 const mongoose = require('mongoose');
 
-// Función asincrónica para conectar a la base de datos MongoDB
+// Definimos una función asincrónica para conectar a la base de datos MongoDB
 const conectarDB = async () => {
-    try{
+    try {
+        // Intentamos conectarnos a la base de datos 'authdb' en localhost (127.0.0.1)
         await mongoose.connect('mongodb://127.0.0.1:27017/authdb', {
             serverSelectionTimeoutMS: 5000, // Tiempo de espera para la selección del servidor
-            socketTimeoutMS: 45000 // Tiempi máximo de inactividad del socket
+            socketTimeoutMS: 45000 // Tiempo máximo de inactividad del socket
         });
-        console.log('MongoDb conectado');
-    } catch (error){
-        console.error('Error al conectar a MongoBd:', error.message);
+
+        // Si la conexión es exitosa, mostramos un mensaje en la consola
+        console.log('MongoDB conectado');
+    } catch (error) {
+        // Si ocurre un error al conectar, mostramos un mensaje de error y salimos del proceso
+        console.error('Error al conectar a MongoDB:', error.message);
         process.exit(1);
     }
 };
 
+// Exportamos la función para que pueda ser utilizada desde otros archivos
 module.exports = conectarDB;
